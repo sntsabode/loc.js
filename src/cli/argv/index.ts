4c5d63b5412343633fd32c412a66bac43b2d4ddf
sntsabode <snts.abode@gmail.com>
2022-01-25 00:36:21 +0200

    feat(cli): make path arg paths arg

--- src/cli/argv/index.ts
+++ src/cli/argv/index.ts
@@ -1,32 +1,21 @@
-import { join } from 'path'
-import { unsafe } from '../../lib/utils'
 import { Argv } from './interfaces'
 import ArgParser from './parser'
 
-export default function fetch_argv() {
-  const argv = new ArgParser<Argv>()
-    .arg('yes', {
-      alias: 'y',
-      type: 'boolean',
-      describe: 'Do not ask any questions.'
-    })
-    .arg('path', {
-      alias: 'p',
-      type: 'string',
-      describe: 'Enter a path loc should work in, relative to the current directory.'
-    })
-    .argv
+const fetch_argv = () => new ArgParser<Argv>()
+  .arg('yes', {
+    alias: 'y',
+    type: 'boolean',
+    describe: 'Do not ask any questions.'
+  })
+  .arg('paths', {
+    alias: 'p',
+    type: 'array',
+    describe: 'Enter a path loc should work in, relative to the current directory.',
+    sanitizer: (p, sup) => p.substring(0, 2) === './'
+      ? sup.staged_args.cwd + '/' + p.substring(2)
+      : sup.staged_args.cwd + '/' + p
+  })
+  .insert('cwd', process.cwd())
+  .argv
 
-  processPathArg(argv)
-
-  return argv
-}
-
-function processPathArg(argv: Argv) {
-  (argv as unsafe).cwd = argv.path
-    ? join(process.cwd(), argv.path.startsWith('./')
-        ? argv.path.substring(2)
-        : argv.path
-      )
-    : process.cwd()
-}
+export default fetch_argv
