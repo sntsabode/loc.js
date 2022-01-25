f5aafc9d8bab295b704eb43ee6927e6e03d70a92
sntsabode <snts.abode@gmail.com>
2022-01-25 11:41:02 +0200

    feat(lib): `nil` type

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,7 @@
 import { createInterface } from 'readline'
 
+export type nil<T = undefined> = undefined | T
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type unsafe = any
 
