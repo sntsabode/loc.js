fe00054375b8273e2f4e5715eed69c5309c67062
sntsabode <snts.abode@gmail.com>
2022-01-25 11:41:28 +0200

    feat(lib): make `loc` a class

--- src/lib/loc/index.ts
+++ src/lib/loc/index.ts
@@ -4,49 +4,56 @@ import { join } from 'path'
 import { ask } from '../utils'
 import { Args, Count } from './interfaces'
 
-export default async function loc(args: Args): Promise<Count> {
-  const count = { c: 0 }
-  await readDir(args.cwd, count, args)
+export default class loc {
+  private args: Args
+  private count: Count = { c: 0 }
 
-  return count
-}
-
-export async function readDir(dir: string, count: Count, args: Args) {
-  for (const entry of readdirSync(dir, { encoding: 'utf-8' })) {
-    const path = join(dir, entry)
-    if (!checkEntry(path, args)) { continue }
-    const stat = lstatSync(path)
-    const [emoji, val] = stat.isDirectory() ? ['📂', 'directory'] : ['📝', 'file']
-
-    console.log(`${emoji} Current ${val}:`, path)
-
-    if (!args.yes) {
-      const a = await ask('loc>')
-      if (a === 's' || a === 'skip' || a === 'i' || a === 'ignore')
-        continue
-    }
-
-    if (stat.isDirectory()) { await readDir(path, count, args) }
-    else if (stat.isFile()) { await countLines(path, count) }
+  constructor(args: Args) {
+    this.args = args
   }
-}
 
-export function checkEntry(fullPath: string, args: Args): boolean {
-  if (!args.paths) { return true }
+  async main(): Promise<Count> {
+    await this.readDir(this.args.cwd)
+    return this.count
+  }
 
-  const i = args.paths.findIndex(p => fullPath.startsWith(p))
-  if (i === -1) { return false }
+  private async readDir(dir: string) {
+    for (const entry of readdirSync(dir, { encoding: 'utf-8' })) {
+      const path = join(dir, entry)
+      if (!this.checkEntry(path)) { continue }
+      const stat = lstatSync(path)
 
-  return true
-}
+      const [emoji, val] = stat.isDirectory() ? ['📂', 'directory'] : ['📝', 'file']
+      console.log(`${emoji} Current ${val}:`, path)
 
-export async function countLines(path: string, count: Count) {
-  return new Promise<void>((resolve) => {
-    createReadStream(path)
-      .on('data', (chunk) => {
-        for (let i = 0; i < chunk.length; ++i)
-          if (chunk[i] == 10) count.c++;
-      })
-      .on('end', () => resolve())
-  })
+      if (!this.args.yes) {
+        const a = await ask('loc>')
+        if (a === 's' || a === 'skip' || a === 'i' || a === 'ignore')
+          continue
+      }
+
+      if (stat.isDirectory()) { await this.readDir(path) }
+      else if (stat.isFile()) { await this.countLines(path) }
+    }
+  }
+
+  private checkEntry(path: string): boolean {
+    if (!this.args.paths) { return true }
+
+    const i = this.args.paths.findIndex(p => path.startsWith(p))
+    if (i === -1) { return false }
+
+    return true
+  }
+
+  private async countLines(path: string) {
+    return new Promise<void>((resolve) => {
+      createReadStream(path)
+        .on('data', (chunk) => {
+          for (let i = 0; i < chunk.length; ++i)
+            if (chunk[i] == 10) this.count.c++;
+        })
+        .on('end', () => resolve())
+    })
+  }
 }
