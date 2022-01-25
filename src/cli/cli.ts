af2157024ce27ca1e387d4fe7eed08acbd842260
sntsabode <snts.abode@gmail.com>
2022-01-25 12:03:39 +0200

    feat(cli): fetch args error handling

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,7 +1,16 @@
-import loc from '../lib/loc'
+import { Argv } from './argv/interfaces'
 import fetch_argv from './argv'
+import loc from '../lib/loc'
 
-new loc(fetch_argv()).main().then(
+let args: Argv
+try {
+  args = fetch_argv()
+} catch (e: unknown) {
+  console.error((e as { message: string }).message ?? e)
+  process.exit(1)
+}
+
+new loc(args).main().then(
   ({ c }) => console.log(c, 'lines counted.'),
-  (e) => console.error(e)
+  (e) => console.error(e.message ?? e)
 )
